2b947486df3875b1eb5a306071380205a8afebf3
Nishith-Savla <nishithsavla005@gmail.com>
2023-09-16 17:09:19 +0530

    Resolve CORS by adding route proxy on frontend

--- backend/app.py
+++ backend/app.py
@@ -17,26 +17,39 @@ from pathlib import Path
 
 # ML
 import xml.etree.ElementTree as ET
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
+
+import netifaces
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import pyshark
+import requests
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS, cross_origin
+from manuf import manuf
+from scapy.all import *
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score
-import sys 
+from sklearn.model_selection import train_test_split
+from werkzeug.utils import secure_filename
 
 from helper import *
 
 app = Flask(__name__)
 mac_parser = manuf.MacParser()
-app.config['UPLOAD_FOLDER'] = 'uploads'
-CORS(app, supports_credentials=True)
+app.config["UPLOAD_FOLDER"] = "uploads"
+cors = CORS(app, support_credentials=True)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
 # Define the analyze endpoint
-@app.route('/analyze', methods=['POST'])
+@app.route("/analyze", methods=["POST"])
+@cross_origin(origin="*", headers=["Content-Type", "Authorization"])
 def analyze():
     # Get the uploaded file from the request object
     print(request.files)
-    pcap_file = request.files['data']
+    pcap_file = request.files["data"]
     # Save the uploaded file to disk in the UPLOAD_FOLDER directory
     filename = secure_filename(pcap_file.filename)
     pcap_path = Path(app.config['UPLOAD_FOLDER']) / filename
@@ -53,30 +66,34 @@ def analyze():
     print("=>", pcap_name)
 
     return_obj = {
-        'connections': connections,
-        'protocol_plots': protocol_counts,
-        'vendor_plots': vendor_plots,
-        'predicted_devices': list(predicted_device)
+        "connections": connections,
+        "protocol_plots": protocol_counts,
+        "vendor_plots": vendor_plots,
+        "predicted_devices": list(predicted_device),
     }
 
     print(return_obj)
-    return jsonify(return_obj)
+    response = jsonify(return_obj)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
 
 
 # Define the home endpoint
-@app.route('/')
+@app.route("/")
 def home():
     interfaces = netifaces.interfaces()
     interfaces = interfaces[::-1]
-    return render_template('index.html', interfaces=interfaces)
+    return render_template("index.html", interfaces=interfaces)
 
 
-@app.route('/graph', methods=['POST'])
+@app.route("/graph", methods=["POST"])
 def graph():
     pcap_file = request.files["pcap-file"]
     filename = secure_filename(pcap_file.filename)
-    pcap_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    pcap_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    pcap_file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    pcap_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
     print(pcap_path)
     pcap_name = os.path.basename(pcap_path)
     capture = pyshark.FileCapture(pcap_path)
@@ -94,7 +111,7 @@ def graph():
         try:
             layers = list(packet.layers)
             # Check if the packet has an Ethernet layer
-            if 'ETH Layer' in str(packet.layers):
+            if "ETH Layer" in str(packet.layers):
                 # Get the source and destination MAC addresses
                 src_mac = packet.eth.src.lower()
                 dst_mac = packet.eth.dst.lower()
@@ -157,16 +174,15 @@ def graph():
             print(f"Error processing packet: {e}")
 
     nodes = list(set(list(edges.keys()) + [k for v in edges.values() for k in v.keys()]))
-    # nodes_data = [{"id": node, "label": f"{node} ({mac_vendor_dict.get(node, 'Unknown Vendor')})"} for node in nodes]
 
     nodes_data = [
         {
             "id": node,
             "label": f"{node} ({html.escape(mac_vendor_dict.get(node, 'Unknown Vendor'))})"
-
         }
         for node in nodes
     ]
+
     edges_data = [
         {"from": src, "to": dst, "label": ", ".join(edge_data["label"])}
         for src, dst_data in edges.items()
@@ -176,37 +192,41 @@ def graph():
         "nodes": nodes_data,
         "edges": edges_data,
         "os_name": os_name,
-        "os_image": os_image
+        "os_image": os_image,
     }
     return graph_data
 
 
 # tcpdump
-@app.route('/start_capture', methods=['POST'])
+@app.route("/start_capture", methods=["POST"])
 def start_capture():
-    interface = request.json['interface']
-    file_name = request.json['file_name']
-    pcap_dir = os.path.join(os.getcwd(), 'pcap')
+    interface = request.json["interface"]
+    file_name = request.json["file_name"]
+    pcap_dir = os.path.join(os.getcwd(), "pcap")
     os.makedirs(pcap_dir, exist_ok=True)
     pcap_file = os.path.join(pcap_dir, file_name)
-    os.system(f'sudo tcpdump -i {interface} -w {pcap_file} &')
-    return jsonify({'message': f'Starting capture on interface {interface} and saving to file {pcap_file}'})
+    os.system(f"sudo tcpdump -i {interface} -w {pcap_file} &")
+    return jsonify(
+        {
+            "message": f"Starting capture on interface {interface} and saving to file {pcap_file}"
+        }
+    )
 
 
-@app.route('/stop_capture', methods=['POST'])
+@app.route("/stop_capture", methods=["POST"])
 def stop_capture():
-    os.system('sudo killall tcpdump')
-    return jsonify({'message': 'Capture stopped.'})
+    os.system("sudo killall tcpdump")
+    return jsonify({"message": "Capture stopped."})
 
 
-@app.route('/cve/<mac>')
+@app.route("/cve/<mac>")
 def cve(mac):
     vendor = mac_parser.get_manuf(mac)
-    pages = request.args.get('pages', default='50', type=int)
+    pages = request.args.get("pages", default="50", type=int)
     cve_list = lookup_cve(vendor, pages)
     return cve_list
 
 
-if __name__ == '__main__':
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+if __name__ == "__main__":
+    os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
     app.run("0.0.0.0", debug=True)
